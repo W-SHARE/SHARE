4a8e0574bc946084e97969de2e3500e7293b2581
hirkawa_yuta <0506.yuuta@gmail.com>
2022-03-23 16:49:29 +0900

    add login action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_22_074732) do
+ActiveRecord::Schema.define(version: 2022_03_23_054557) do
 
   create_table "courses", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -86,6 +86,7 @@ ActiveRecord::Schema.define(version: 2022_03_22_074732) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
