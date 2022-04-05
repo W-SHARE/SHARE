26d12bebe63a6eb006db985bcd6131f922a7a4e5
hiruyu56 <99026117+hiruyu56@users.noreply.github.com>
2022-04-05 13:18:49 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_23_054557) do
+
 
   create_table "courses", force: :cascade do |t|
     t.datetime "created_at", null: false
